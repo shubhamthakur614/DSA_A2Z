e78d2fb6819196c8f74edaaea56f8d870199616e
Shubham Thakur <shubham.thakur@Yash.com>
2023-04-15 22:51:02 +0530

    Basic sorting file added 15-april

--- DSA_A2Z/src/com/array/Test.java
+++ DSA_A2Z/src/com/array/Test.java
@@ -1,26 +1,27 @@
 package com.array;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Test {
 	public static void main(String[] args) {
-
-		int N = 5;
-
-		for (int i = 0; i < N; i++) {
-			// For printing the spaces before stars in each row
-			for (int j = 0; j < i; j++) {
+		
+		int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=1;j<=n-i-1;j++)
+			{
 				System.out.print(" ");
 			}
-
-			// For printing the stars in each row
-			for (int j = 0; j < 2 * N - (2 * i + 1); j++) {
-
+			for(int j=0;j<2*i+1;j++)
+			{
 				System.out.print("*");
+				
 			}
+			for(int j=1;j<=n-i-1;j++) {
+				System.out.print(" ");
+			}
+				
 			System.out.println();
 		}
+
 	}
 }
+
