dde67e77fda173669094208674a5568ce608b037
Shubham Thakur <shubham.thakur@Yash.com>
2023-07-23 17:09:29 +0530

    23 july 2023

--- DSA_A2Z/src/com/test/Test.java
+++ DSA_A2Z/src/com/test/Test.java
@@ -2,56 +2,61 @@ package com.test;
 
 public class Test {
 	public static void main(String[] args) {
-		int[] arr = { 1, 3, 5, 7, 7, 7, 10 };// 3-5=2+1=3
-		int target = 7;
+		int[] arr = { 1, 1, 1, 2, 2,3,3,5,5 };// 3-5=2+1=3
+		int target = 3;
 		// frequency of target using ub and lb
-		int frequency = frequency(arr,target);
-		System.out.println(frequency);
+		int frequencyCount = frequencyCount(arr, target);
+		System.out.println(frequencyCount);
 
 	}
-	public static int frequency(int[] arr,int target)
-	{
-		int lb = lb(arr, target);
-		int ub = ub(arr, target);
-		if(arr[lb]!=target)
-			return -1;
-		else {
-			return ub-lb;
-		}
+
+	public static int frequencyCount(int[] arr, int x) {
+		int[] firstAndLastOcceranceFreq = firstAndLastOccerance(arr,x);
+		if(firstAndLastOcceranceFreq[0]==-1)return 0;
+		return (firstAndLastOcceranceFreq[1]-firstAndLastOcceranceFreq[0]+1);
+
 	}
 
-	public static int lb(int[] arr, int target) {
+	public static int[] firstAndLastOccerance(int[] arr, int x) {
+		int firstOccerance = firstOccerance(arr, x);
+		if (firstOccerance == -1)
+			return new int[] { -1, -1 };
+
+		int lastOccerance = lastOccerance(arr, x);
+		return new int[] { firstOccerance, lastOccerance };
+	}
+
+	public static int firstOccerance(int[] arr, int target) {
 		int low = 0;
 		int high = arr.length - 1;
 		int ans = -1;
 		while (low <= high) {
 			int mid = low + (high - low) / 2;
-			if (arr[mid] >= target) {
+			if (arr[mid] == target) {
 				ans = mid;
 				high = mid - 1;
 				continue;
-			}
-			if (target > +arr[mid]) {
+			} else if (arr[mid] < target) {
 				low = mid + 1;
 			} else {
 				high = mid - 1;
 			}
 		}
 		return ans;
+
 	}
-	
-	public static int ub(int[] arr, int target) {
+
+	public static int lastOccerance(int[] arr, int target) {
 		int low = 0;
 		int high = arr.length - 1;
 		int ans = -1;
 		while (low <= high) {
 			int mid = low + (high - low) / 2;
-			if (arr[mid] >= target) {
+			if (arr[mid] == target) {
 				ans = mid;
 				low = mid + 1;
 				continue;
-			}
-			if (target > +arr[mid]) {
+			} else if (arr[mid] < target) {
 				low = mid + 1;
 			} else {
 				high = mid - 1;
