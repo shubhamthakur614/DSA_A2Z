a476f5a921eb4c9c790c07e5fd6ff08b2193c845
shubham thakur <shubhamthakur614@gmail.com>
2025-04-03 19:33:57 +0530

    optimal code for sort 0's ,1's and 2's added

--- DSA_A2Z/src/com/linkedlist/Sort_0_1_2.java
+++ DSA_A2Z/src/com/linkedlist/Sort_0_1_2.java
@@ -13,7 +13,7 @@ public class Sort_0_1_2 {
 			this.next = null;
 		}
 	}
-	// bruteforce approach
+	// bruteforce approach( TC-> O(2N) , SC-> O(1))
 
 	/*
 	 * Intution :
@@ -24,7 +24,7 @@ public class Sort_0_1_2 {
 	 * count till null
 	 * 
 	 */
-	static Node segregate(Node head) {
+	public static Node segregate(Node head) {
 		if (head == null) {
 			return head;
 		}
@@ -59,6 +59,63 @@ public class Sort_0_1_2 {
 		return head;
 	}
 	
+	
+	
+	//Optimal approach (TC-> O(N) SC-> O(1) )
+	
+	/* Here we use the dummy node and attach dummy.next to curr node and create 3 
+	 * segrigated LL and we have to join them one after another
+	 * 
+	 * 
+	 */
+	public static Node segregate_0_1_2(Node head) {
+		
+		// Create three dummy nodes to handle the three partitions for 0, 1, and 2
+	    Node zeroHead = new Node(-1);  // Dummy node for 0's
+	    Node oneHead = new Node(-1);   // Dummy node for 1's
+	    Node twoHead = new Node(-1);   // Dummy node for 2's
+	    
+	    Node zero = zeroHead;   // Pointer to track the end of 0's list
+	    Node one = oneHead;     // Pointer to track the end of 1's list
+	    Node two = twoHead;     // Pointer to track the end of 2's list
+	    
+	    Node curr = head;  // Start traversing the original list
+	    
+	    // Traverse the original list and partition the nodes based on their value
+	    while (curr != null) {
+	        if (curr.data == 0) {
+	            zero.next = curr;
+	            zero = zero.next;
+	        } else if (curr.data == 1) {
+	            one.next = curr;
+	            one = one.next;
+	        } else {  // curr.data == 2
+	            two.next = curr;
+	            two = two.next;
+	        }
+	        curr = curr.next;
+	    }
+	    
+	    // Now connect the three partitions together:
+	    // 1. Connect the end of the 0's list to the beginning of the 1's list
+	    if (oneHead.next != null) {
+	        zero.next = oneHead.next;
+	    } else {
+	        zero.next = twoHead.next;  // No 1's, connect directly to 2's list
+	    }
+	    
+	    // 2. Connect the end of the 1's list to the beginning of the 2's list
+	    one.next = twoHead.next;
+	    
+	    // 3. The last node of the 2's list should point to null to terminate the list
+	    two.next = null;
+	    
+	    // The head of the final list is the node after the dummy node in zeroHead
+	    head = zeroHead.next;
+	    
+	    return head;
+	}
+	
 	// Function to print the linked list
 	static void printLinkedList(Node head) {
 		Node temp = head;
@@ -81,7 +138,7 @@ public class Sort_0_1_2 {
 		head.next.next.next.next.next.next.next = new Node(0);
 		
 		printLinkedList(head);
-		head=segregate(head);
+		head=segregate_0_1_2(head);
 		printLinkedList(head);
 		
 	}
